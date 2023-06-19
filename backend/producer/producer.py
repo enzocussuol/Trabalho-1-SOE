90577718aedbfb3bc1144918d59abd3f32061c21
enzocussuol <enzo@allsystem.com.br>
2023-06-19 09:36:16 -0300

    Adicionando novos topicos

--- backend/producer/producer.py
+++ backend/producer/producer.py
@@ -5,6 +5,7 @@ import time
 import OpenMeteoClient as omc
 
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'],value_serializer=lambda m: json.dumps(m).encode('utf-8'))
+producerString = KafkaProducer(bootstrap_servers=['localhost:9092'])
 
 def on_send_success(record_metadata):
     print(f"Evento enviado com sucesso no topico {record_metadata.topic} na partição {record_metadata.partition} com offset {record_metadata.offset}")
@@ -21,7 +22,24 @@ while True:
     # Get Info on OpenMeteo
     data = client.get_locations_weather()
 
+    for location in data['data']:
+        producerString.send('temperature',
+                            value=bytes(str(location['data']['temperature']),encoding="utf-8"),
+                            key=bytes(location['code'], encoding='utf-8')).add_callback(on_send_success).add_errback(on_send_error)
+
+        producerString.send('wind',
+                            value=bytes(str(location['data']['windspeed']),encoding="utf-8"),
+                            key=bytes(location['code'], encoding='utf-8')).add_callback(on_send_success).add_errback(on_send_error)
+
+        producerString.send('uv',
+                            value=bytes(str(location['data']['uv_index_max']),encoding="utf-8"),
+                            key=bytes(location['code'], encoding='utf-8')).add_callback(on_send_success).add_errback(on_send_error)
+
+        producerString.send('precipitation',
+                            value=bytes(str(location['data']['precipitation']),encoding="utf-8"),
+                            key=bytes(location['code'], encoding='utf-8')).add_callback(on_send_success).add_errback(on_send_error)
+
     producer.send('clima', data).add_callback(on_send_success).add_errback(on_send_error)
 
     #Sleep
-    time.sleep(10)
+    time.sleep(10000)
