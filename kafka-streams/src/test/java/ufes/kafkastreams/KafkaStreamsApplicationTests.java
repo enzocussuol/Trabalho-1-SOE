45f2cf0c5353f9b6229068ea750f1a8141e671db
enzocussuol <enzo@allsystem.com.br>
2023-07-01 12:46:52 -0300

    :sparkles: Adicionando projeto java kafka-streams

---
+++ kafka-streams/src/test/java/ufes/kafkastreams/KafkaStreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package ufes.kafkastreams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
